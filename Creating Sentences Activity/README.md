7e97abadd755ee45356f5d0635520b54a7109490
Mike Piech <mpiech@alum.mit.edu>
2021-12-18 21:58:24 -0800

    new file

--- Creating Sentences Activity/README.md
+++ Creating Sentences Activity/README.md
@@ -1,1 +1,2 @@
 # Creating Sentences Activity
+This function turns an array of words into a sentence and prints it.
---
+++ Creating Sentences Activity/creating_sentences.js
@@ -0,0 +1,25 @@
+// TODO: Create an array variable called "words" with the following elements: 'This', 'is', 'JavaScript', 'Bootcamp!'
+// YOUR CODE GOES HERE
+var mywords = ["This", "is", "JavaScript", "Bootcamp!"];
+
+// TODO: Create an empty string variable called "sentence".
+// YOUR CODE GOES HERE
+var sentence = "";
+
+// TODO: 
+// 1. Create a function called "createSentence" that takes an array as an argument.
+// 2. In the function use a for loop to iterate through each word element of the array.
+// 3. Add each word to the "sentence" variable.
+// 4. Return the "sentence".
+// YOUR CODE GOES HERE
+function createSentence(words) {
+    for (var wordindex in words) { 
+        var word = words[wordindex];
+        sentence += word + " ";
+    }
+    return sentence;
+}
+
+// TODO: Call the function "createSentence" using the console.log method.
+// YOUR CODE GOES HERE
+console.log(createSentence(mywords));
