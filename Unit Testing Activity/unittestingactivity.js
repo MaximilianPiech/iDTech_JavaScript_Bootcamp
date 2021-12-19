d1ce811e518a4c111cbf1ae7223b514fbe480de3
Mike Piech <mpiech@alum.mit.edu>
2021-12-19 10:55:01 -0800

    new file

--- Unit Testing Activity/unittestingactivity.js
+++ Unit Testing Activity/unittestingactivity.js
@@ -3,12 +3,12 @@ function Greet(name){
     if (name == null) {
         console.log("Hello, there!");
     }
-    if (name.length > 1) {
+    if (Array.isArray(name)) {
         console.log("Hello" + name[0] + name[1] + name[2] + name[3]);
     }
-    else
-    {
-    if (name == name.toUpperCase())
+    else if (name == name.toUpperCase()) {
         console.log("HELLO " + name + "!");
     }
 }
+
+Greet("Joseph");
