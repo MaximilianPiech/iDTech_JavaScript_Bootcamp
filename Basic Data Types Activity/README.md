7b5ee371bdb612ea472b53b4feb2805583bbf2fd
Mike Piech <mpiech@alum.mit.edu>
2021-12-18 21:49:59 -0800

    new file

--- Basic Data Types Activity/README.md
+++ Basic Data Types Activity/README.md
@@ -1,1 +1,2 @@
 # Working with Basic Data Types Activity
+I created involved strings, an array, and a boolean in this story.
---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,35 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Hello traveler!";
+let name = "H4CK3R";
+let job = "engineer";
+let town = "Sunnyvale";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = "2035";
+let age = "18";
+let telephone = "4085558932";
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let importantTools = ['soldering iron', 'wire cutter', 'terminal crimper'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasFavoriteTool = false;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + " I am " + name + "!");
+console.log("I am an " + job + ".");
+console.log("I am from " + town + ".");
+console.log("It is the year " + year + ".");
+console.log("I am " + age + " years old.");
+console.log("My telephone number is " + telephone + "." + "\n");
+console.log("Is my " + importantTools[1] + " here?")
+console.log(hasFavoriteTool);
+console.log("Will you help me find it?" + "\n");
+console.log("-->Yes    No");
+console.log("Yes.");
+console.log("Traveler, your quest has begun...");
